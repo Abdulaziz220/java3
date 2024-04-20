dea3cef3fe964aede4f35639055763921e9ddc63
Your Name <you@example.com>
2024-04-20 12:48:54 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+alert("Hozir sizga random sonlar chiqishni boshlaydi!");
+
+let a;
+let b;
+let c;
+let  d;
+let  p;
+
+a = Math.trunc(20 * Math.random(10))
+c = 5;
+b = a * c;
+d = 2;
+p = b / c;
+
+
+alert("A Random son = " + a);
+alert("Tasodifiy soni 5 kopaytiring?");
+alert("Tasodifiy son = " + a + " ni = " + c + "ga kopaytirdig va = " + p + "soni chiqardik");
